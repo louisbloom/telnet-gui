c84c208efb7ba5bca6dadfa4198326840947508a
Thomas Christensen <thomasc1971@hotmail.com>
2025-12-21 18:40:37 +0700

    feat: Add carrionfields.net lisp scripts folder
    - Move practice-simple.lisp (formerly slash.lisp) to carrionfields.net/
    - Add seaport.lisp with ship arrival triggers converted from TinTin++

---
+++ lisp/carrionfields.net/seaport.lisp
@@ -0,0 +1,49 @@
+;; TinTin++ State File
+;; Seaport ship triggers for Carrion Fields (carrionfields.net)
+;;
+;; Converted from original TinTin++ seaport.conf
+;; Each action triggers once when a ship arrives, then removes itself.
+;;
+;; Usage: (load "lisp/carrionfields.net/seaport.lisp")
+;;    or: #load lisp/carrionfields.net/seaport.lisp
+
+;; Aliases
+
+;; Variables
+(hash-set! *tintin-variables* "seaport" "true")
+
+;; Highlights
+
+;; Actions
+(hash-set! *tintin-actions* "With a hiss that echoes eerily over the Seaport, a dark, mysterious ship" (list "#unaction {With a hiss that echoes eerily over the Seaport, a dark, mysterious ship};sseen;op n;n;l Maleavia;sswwnn" 5))
+(hash-set! *tintin-actions* "Showing off like a peacock, the arial-manned 'Sapphire Wave'" (list "#unaction {Showing off like a peacock, the arial-manned 'Sapphire Wave'};nneee;l captain;op d;d;l portholes;l fake;g all fake;u;wwwss" 5))
+(hash-set! *tintin-actions* "Sending a chorus of circus music over the dock, the 'Aquavia Montage'" (list "#unaction {Sending a chorus of circus music over the dock, the 'Aquavia Montage'};sseen;l captain;op w;op d;op n;wedunwes;swwnn" 5))
+(hash-set! *tintin-actions* "The glimmering crystal felucca 'The Golden Pearl'" (list "#unaction {The glimmering crystal felucca 'The Golden Pearl'};een;l anxchizrepp;op d;du;sww" 5))
+(hash-set! *tintin-actions* "The 'Rapacious Reaver', a slim black barge with a deep hull" (list "#unaction {The 'Rapacious Reaver', a slim black barge with a deep hull};sseen;l captain;op d;du;swwnn" 5))
+(hash-set! *tintin-actions* "'Aarn's Holy Aler', the food and alcohol merchant ship" (list "#unaction {'Aarn's Holy Aler', the food and alcohol merchant ship};nneee;l captain;wwwss" 5))
+(hash-set! *tintin-actions* "The textile merchant ship 'The Laughing Lord' ports" (list "#unaction {The textile merchant ship 'The Laughing Lord' ports};nneee;l captain;wwwss" 5))
+(hash-set! *tintin-actions* "Her headmast decorated with a huge statue of a lily, the 'Thieving Lily'" (list "#unaction {Her headmast decorated with a huge statue of a lily, the 'Thieving Lily'};nneee;l captain;op n;nsud;wwwss" 5))
+(hash-set! *tintin-actions* "People shy away from the south deck as a bloody galleon marked 'Sheshani'" (list "#unaction {People shy away from the south deck as a bloody galleon marked 'Sheshani'};sseen;l captain;swwnn" 5))
+(hash-set! *tintin-actions* "Her sails glowing a holy white, the 'Incarnadine Spray'" (list "#unaction {Her sails glowing a holy white, the 'Incarnadine Spray'};een;l captain;sww" 5))
+(hash-set! *tintin-actions* "The Sirine's Dirge docks at port quietly" (list "#unaction {The Sirine's Dirge docks at port quietly};ees;l captain;nww" 5))
+(hash-set! *tintin-actions* "A dark house boat with the words 'Coeur Rouge'" (list "#unaction {A dark house boat with the words 'Coeur Rouge'};sseen;l graygrass;swwnn" 5))
+(hash-set! *tintin-actions* "Taking up a huge amount of the docking space, the Sultan's War Barge 'Hammarley'" (list "#unaction {Taking up a huge amount of the docking space, the Sultan's War Barge 'Hammarley'};sseen;l captain;op n;op d;duns;swwnn" 5))
+(hash-set! *tintin-actions* "Ever loyal to the Empire, the 'Buzzing Hornet'" (list "#unaction {Ever loyal to the Empire, the 'Buzzing Hornet'};sseee;op n;n;l captain;s;wwwnn" 5))
+(hash-set! *tintin-actions* "With a bloody cry of war, the pirates aboard the 'Redd Iaage - Valguarnera's Judgment'" (list "#unaction {With a bloody cry of war, the pirates aboard the 'Redd Iaage - Valguarnera's Judgment'};nneee;l mondlinfir;op u;ud;wwwss" 5))
+(hash-set! *tintin-actions* "The jade elven skiff 'Jade Kiss' docks at the merchant port" (list "#unaction {The jade elven skiff 'Jade Kiss' docks at the merchant port};nneee;l captain;wwwss" 5))
+(hash-set! *tintin-actions* "Bringing a shipment of fish, foods and ales to the Seaport, the 'Dancing Renna'" (list "#unaction {Bringing a shipment of fish, foods and ales to the Seaport, the 'Dancing Renna'};sseen;l captain;op d;du;swwnn" 5))
+(hash-set! *tintin-actions* "A sinister black ship with a nasty barbed railing docks at the darkened port.  The words 'Grurk's Rising'" (list "#unaction {A sinister black ship with a nasty barbed railing docks at the darkened port.  The words 'Grurk's Rising'};sseen;op n;op d;dun;l captain;s;swwnn" 5))
+(hash-set! *tintin-actions* "People look on in disgust as 'The Crooked Shanty'" (list "#unaction {People look on in disgust as 'The Crooked Shanty'};ees;l captain;nww" 5))
+(hash-set! *tintin-actions* "A sparkling, beautiful yacht pulls up to the adventurer's port and begins selling expensive tickets to very rich-looking customers.  The words 'Horizon's Seeker'" (list "#unaction {A sparkling, beautiful yacht pulls up to the adventurer's port and begins selling expensive tickets to very rich-looking customers.  The words 'Horizon's Seeker'};een;l captain;op n;op w;ns;w;op n;ns;e;sww" 5))
+(hash-set! *tintin-actions* "The 'Gray Monk' glides smoothly to port at the south dock" (list "#unaction {The 'Gray Monk' glides smoothly to port at the south dock};sseen;l head;swwnn" 5))
+(hash-set! *tintin-actions* "A simple fisherman's skiff, the 'Delving Shark'" (list "#unaction {A simple fisherman's skiff, the 'Delving Shark'};nneee;l elderly;wwwss" 5))
+(hash-set! *tintin-actions* "Bold as can be, a massive, three-hulled trimaran named 'The Crimson Fire'" (list "#unaction {Bold as can be, a massive, three-hulled trimaran named 'The Crimson Fire'};een;l captain;sww" 5))
+(hash-set! *tintin-actions* "With the sound of drunken laughter echoing from her deck, the rumrunner ship 'Thirsty Bear'" (list "#unaction {With the sound of drunken laughter echoing from her deck, the rumrunner ship 'Thirsty Bear'};nneee;l captain;op d;du;wwwss" 5))
+(hash-set! *tintin-actions* "A sleek black ship without markings dances through the waters of the night, barely illuminated by the moon.  She moves like an onyx dream" (list "#unaction {A sleek black ship without markings dances through the waters of the night, barely illuminated by the moon.  She moves like an onyx dream};wwwwwwssseee;op s;s;op s;s;op s;s;e;op n;n;l alchemist;ssw;nnnwwwnnneeeeee" 5))
+(hash-set! *tintin-actions* "A dark ship with the white words 'Muse's Throne' on her hull glides past the Seaport" (list "#unaction {A dark ship with the white words 'Muse's Throne' on her hull glides past the Seaport};wwwwwwssseee;op s;s;op s;s;op s;s;e;op n;op d;dun;l devil;l cantydine;ssw;nnnwwwnnneeeeee" 5))
+(hash-set! *tintin-actions* "The severe missionary longship 'Kraken's Caress'" (list "#unaction {The severe missionary longship 'Kraken's Caress'};sseen;l kneeling;l half-drow;swwnn" 5))
+
+;; Settings
+(set! *tintin-speedwalk-enabled* #t)
+(set! *tintin-speedwalk-diagonals* #f)
+(set! *tintin-enabled* #t)
